4d2aca6e53c1cf8c53f85ddf7002853189ecc82d
chibitachibita <dizzy.xxx.666@gmail.com>
2023-12-21 16:21:46 +0000

    flutterアップグレード、画面遷移追加(login→トップページ)

---
+++ lib/view/qualificationmgTopPage.dart
@@ -0,0 +1,15 @@
+import 'package:qualificationmg/importer.dart';
+
+class QualificationmgTopPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('上に表示される'),
+      ),
+      body: Center(
+        child: Text('まんなか'),
+      ),
+    );
+  }
+}
