a08beebcf86f7d602e35c571cff39983d03502d9
chibitachibita <dizzy.xxx.666@gmail.com>
2023-12-24 11:48:55 +0000

    動かなくなっちゃったけど登録・ログイン処理を切り分けたから一時コミット

---
+++ lib/firebase/firebase.dart
@@ -0,0 +1,39 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+String newUserEmail = '';
+String newUserPassword = '';
+String loginUserEmail = '';
+String loginUserPassword = '';
+
+Future<String?> firebaseRegist() async {
+  // ユーザー登録
+  String infoText = '';
+  try {
+    final FirebaseAuth auth = FirebaseAuth.instance;
+    final UserCredential result = await auth.createUserWithEmailAndPassword(
+      email: newUserEmail,
+      password: newUserPassword,
+    );
+    final User user = result.user!;
+    infoText = "ユーザー登録を完了しました。登録したメールアドレスは${user.email}です";
+    return infoText;
+  } catch (e) {
+    infoText = "ユーザー登録時にエラーが発生しました";
+  }
+}
+
+Future<String?> firebaseLogin() async {
+  String infoText = '';
+  try {
+    final FirebaseAuth auth = FirebaseAuth.instance;
+    final UserCredential result = await auth.signInWithEmailAndPassword(
+      email: loginUserEmail,
+      password: loginUserPassword,
+    );
+    final User user = result.user!;
+    //infoText = "ログインに成功しました。ログインメールアドレスは${user.email}です";
+    return infoText;
+  } catch (e) {
+    infoText = "ログイン時にエラーが発生しました";
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
+import 'package:qualificationmg/firebase/firebase.dart';
 import 'package:qualificationmg/importer.dart';
 
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:qualificationmg/firebase_options.dart';
 import 'view/qualificationmgTopPage.dart';
 
@@ -31,15 +31,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  //入力されたメールアドレスを入れるデータ
-  String newUserEmail = '';
-  //入力されたパスワードを入れるデータ
-  String newUserPassword = '';
-  // 登録・ログインに関する情報を表示するデータ
   String infoText = '';
-
-  String loginUserEmail = '';
-  String loginUserPassword = '';
 
   @override
   Widget build(BuildContext context) {
@@ -77,24 +69,10 @@ class _MyHomePageState extends State<MyHomePage> {
                 ElevatedButton(
                   // Firebaseと通信するのでasyncが必要
                   onPressed: () async {
-                    //ユーザー登録が無事Firebaseに登録できた場合の処理
-                    try {
-                      final FirebaseAuth auth = FirebaseAuth.instance;
-                      final UserCredential result =
-                          await auth.createUserWithEmailAndPassword(
-                        email: newUserEmail,
-                        password: newUserPassword,
-                      );
-
-                      final User user = result.user!;
-                      setState(() {
-                        infoText = "ユーザー登録を完了しました。登録したメールアドレスは${user.email}です";
-                      });
-                    } catch (e) {
-                      setState(() {
-                        infoText = "ユーザー登録時にエラーが発生しました";
-                      });
-                    }
+                    Future<String?> infoText = firebaseRegist();
+                    setState(() {
+                      infoText;
+                    });
                   },
                   child: Text('Sign Up'),
                 ),
@@ -121,16 +99,13 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
                 ElevatedButton(
                   onPressed: () async {
-                    try {
+                    Future<String?> infoText = firebaseRegist();
+                    if (infoText == '') {
                       Navigator.push(
                         context,
                         MaterialPageRoute(
                             builder: (context) => QualificationmgTopPage()),
                       );
-                    } catch (e) {
-                      setState(() {
-                        infoText = 'ログイン時にエラーが発生しました';
-                      });
                     }
                   },
                   child: Text('Login'),
