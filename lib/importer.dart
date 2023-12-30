9b5f6127f23962aad9323b2ee8b41f88429816bf
chibitachibita <dizzy.xxx.666@gmail.com>
2023-12-30 22:37:40 +0900

    firebase見直し

--- lib/importer.dart
+++ lib/importer.dart
@@ -6,4 +6,6 @@ export 'materialthemebuilder/color_schemes.g.dart';
 
 // firebase
 export 'package:firebase_core/firebase_core.dart';
-export 'package:qualificationmg/firebase_options.dart';
+export 'package:qualificationmg/firebase/firebase_options.dart';
+export 'package:firebase_analytics/firebase_analytics.dart';
+export 'package:firebase_crashlytics/firebase_crashlytics.dart';
